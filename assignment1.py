c662106487b4fc42e0ceab6a6b0d35e8beb34cc0
AdamVSobczyk <128526719+AdamVSobczyk@users.noreply.github.com>
2026-09-22 16:03:10 +0200

    feat: oppgave 1
    alle prints for første oppgave

--- assignment1.py
+++ assignment1.py
@@ -1,5 +1,46 @@
 import pandas as pd
 
-alz_data = pd.read_csv("handwriting-alzheimers.csv", header=0, sep=",")
+sd_data = pd.read_csv("smoking_drinking.csv", header=0, sep=",")
 
-print(alz_data)
+# 1a
+# First rows
+print(sd_data.head())
+print("\n\n\n")
+
+# Summary statistics
+print(sd_data.describe())
+print("\n\n\n")
+
+# Data types
+print(sd_data.dtypes)
+print("\n\n\n")
+
+
+# 1b
+# Missing values
+print(sd_data.isnull().sum())
+print("\n\n\n")
+
+# Unique values
+print(sd_data['sex'].unique())
+print(sd_data['DRK_YN'].unique())
+print(sd_data['SMK_stat_type_cd'].unique())
+print("\n\n\n")
+
+# Outliers (for height)
+Q1 = sd_data['waistline'].quantile(0.10)
+Q3 = sd_data['waistline'].quantile(0.90)
+IQR = Q3 - Q1
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+outliers = sd_data[(sd_data['waistline'] < lower_bound) | (sd_data['waistline'] > upper_bound)]
+print(f"Number of outliers in waistline: {len(outliers)}")
+
+
+
+# Scatter plot
+"""
+sd_data.plot(kind='scatter', x = 'SBP', y = 'DBP')
+plt.show()
+"""
+# Noen verdier med sykt høye max verdier (som 999 waistline)
